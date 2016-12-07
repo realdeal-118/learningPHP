d66071b742fe9b05cb75afe614582677647dcb3a
realdeal-118 - Leotis Fields <realdeal118@hotmail.com>
2016-12-07 04:04:43 -0500

    Changed the border Added labels for "First Name", "Last Name", "Email" Added "padding" to the bottom.

--- Learnphp1/index.php
+++ Learnphp1/index.php
@@ -12,9 +12,15 @@
 
     <style>
         #emailForm{
-            border:dotted black ;
+            border:2px double blue;
             border-radius: 10px;
             margin-top:20px;
+            padding-bottom:10px;
+            
+        }
+
+        #title{
+            text-align:center;
         }
     </style>
 </head>
@@ -28,15 +34,27 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 col-md-offset-3" id="emailForm">
-                    <h1> My Email Form</h1>
+                    <h1 id="title"> My Email Form</h1>
 
                     
-                        <div class="row">
+                      
                             <form method="POST">
+
                                 <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" placeholder="Name" />
+                                    <label for="firstname">First Name</label>
+                                    <input type="text" class="form-control" id="firstName" placeholder="First Name" />
                                 </div>
+
+                                <div class=form-group>
+                                    <label for="lastname">Last Name</label>
+                                    <input type="text" class="form-control" id="lastName" placeholder="Last Name" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Email" />
+                                </div>
+
                                 <div class="form-group">
                                     <label for="telNumber">Tel Number</label>
                                     <input type="tel" class="form-control" id="telNumber" placeholder="Tel Number" />
