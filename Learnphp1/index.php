0451a5ea199f61aa7fe2775dfa13c60987b5ddce
realdeal-118 - Leotis Fields <realdeal118@hotmail.com>
2016-12-08 04:49:36 -0500

    I added server side validation to my form using PHP. I added  emailing capabilities to the form.

--- Learnphp1/index.php
+++ Learnphp1/index.php
@@ -1,3 +1,56 @@
+<?php
+
+if($_POST["Submit"]){
+
+
+    if(!$_POST['firstName']){
+        $error="<br />Please enter your first name";
+    }
+
+    if(!$_POST['lastName']){
+        $error.="<br />Please enter your last name";
+    }
+
+    if(!$_POST['email']){
+        $error.="<br />Please enter your email address";
+    }
+
+    if(!$_POST['telNumber']){
+        $error.="<br />Please enter your telephone number";
+    }
+
+    if(!$_POST['comment']){
+        $error.="<br />Please enter your comment";
+    }
+
+    if($_POST['email']!="" and !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+
+        $error.="<br />Please enter a valid email address";
+    }
+    if($error){
+
+        $result='<div class="alert alert-danger"><strong>There were error(s) in your form:</strong>'.$error.'</div>';
+    }
+    else{
+        if(mail("leotis.fields@yahoo.com", "Comment from Website","Name: ".$_POST."
+            
+            Email: ".$_POST['email']."
+            Comment: ".$_POST['comment'])) {
+            
+            $result='<div class="alert alert-success"><strong>Thank you!</strong> I\'ll be in touch.</div>';
+        }
+        else{
+            
+            $result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
+        }
+    }
+
+}
+
+?>
+
+
+
 <!doctype html>
 <html>
 <head>
@@ -16,7 +69,7 @@
             border-radius: 10px;
             margin-top:20px;
             padding-bottom:10px;
-            
+
         }
 
         #title{
@@ -26,59 +79,59 @@
 </head>
 <body>
     <div>
-        <?php
 
-
-
-        ?>
         <div class="container">
             <div class="row">
                 <div class="col-md-6 col-md-offset-3" id="emailForm">
                     <h1 id="title"> My Email Form</h1>
 
-                    
-                      
+                     <?php
+
+                     echo $result;
+
+                    ?>
+                        <p class="lead">Please get in touch, I will get back to you soon.</p>
+
                             <form method="POST">
 
                                 <div class="form-group">
                                     <label for="firstname">First Name</label>
-                                    <input type="text" class="form-control" id="firstName" placeholder="First Name" />
+                                    <input type="text" class="form-control" name="firstName" placeholder="First Name" />
                                 </div>
 
                                 <div class=form-group>
                                     <label for="lastname">Last Name</label>
-                                    <input type="text" class="form-control" id="lastName" placeholder="Last Name" />
+                                    <input type="text" class="form-control" name="lastName" placeholder="Last Name" />
                                 </div>
 
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" placeholder="Email" />
+                                    <input type="email" class="form-control" name="email" placeholder="Email" />
                                 </div>
 
                                 <div class="form-group">
                                     <label for="telNumber">Tel Number</label>
-                                    <input type="tel" class="form-control" id="telNumber" placeholder="Tel Number" />
-
+                                    <input type="tel" class="form-control" name="telNumber" placeholder="Tel Number" />
                                 </div>
-                                <button type="submit" class="btn btn-default">Submit</button>
 
+                                <div class="form-group">
+                                    <label for="comment">Your Comment</label>
+                                    <textarea class="form-control" name="comment" placeholder="Enter your comment here"></textarea>
+                                </div>
 
-
-
-
+                                <input type="submit" name="Submit" class="btn btn-success btn-lg" value="Submit" />
+                                <!--<button type="submit" class="btn btn-success btn-lg">Submit</button>-->
 
                             </form>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
         <!--Form will submit using the Post Method-->
-        
 
-     
+
+
 
 
 
